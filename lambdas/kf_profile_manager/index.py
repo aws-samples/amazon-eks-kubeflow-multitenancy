0b85ea2cea5c2d9298090ca66af3214a46b29316
Jose Alfredo Castro Nunes <nunesjos@amazon.lu>
2021-10-21 16:21:29 +0200

    added proper logging

--- lambdas/kf_profile_manager/index.py
+++ lambdas/kf_profile_manager/index.py
@@ -3,11 +3,8 @@ import boto3
 import eks_token
 from kubernetes import client, config
 from kubernetes.client.exceptions import ApiException
-from pprint import pprint
-import inspect
 import socket
 import os
-import sys
 import logging
 import requests
 import time
@@ -23,33 +20,29 @@ def is_port_open(host, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex((host, port)) == 0
 
-#generic variables
+
+# generic variables
 
 # connectivity: get the api_client
-#cluster_name = os.environ['clusterName']
-#cognito_group_field = os.environ['cognitoGroupField']
-cluster_name = "kubeflow-ts"
-cognito_group_field = "custom:groups"
+cluster_name = os.environ['clusterName']
+cognito_group_field = os.environ['cognitoGroupField']
+# cluster_name = "kubeflow-ts"
+# cognito_group_field = "custom:groups"
 cluster_info = boto3.client('eks').describe_cluster(name=cluster_name)['cluster']
 ekstoken = eks_token.get_token(cluster_name=cluster_name)['status']['token']
 cluster_host = cluster_info['endpoint'][8:]
 eks_endpoint = cluster_info['endpoint']
 
-
-
 if is_port_open(cluster_host, 443):
-    print(f"EKS API Endpoint accessible: https://{cluster_host}:443")
+    logger.info(f"EKS API Endpoint accessible: https://{cluster_host}:443")
 else:
-    print(f"EKS API Endpoint NOT accessible: https://{cluster_host}:443")
-
-
+    logger.info(f"EKS API Endpoint NOT accessible: https://{cluster_host}:443")
 
 conf = client.Configuration()
 conf.host = cluster_info['endpoint']
 conf.verify_ssl = False
 conf.api_key = {"authorization": "Bearer " + ekstoken}
 api_client = client.ApiClient(conf)
-
 
 
 def get_ns():
@@ -60,7 +53,8 @@ def get_ns():
             ns_list.append(ns.metadata.name)
         return ns_list
     except ApiException as e:
-        print("Exception when calling CoreV1Api->list_namespace: %s\n" % e)
+        logger.info("Exception when calling CoreV1Api->list_namespace: %s\n" % e)
+
 
 def apply_rolebinding(user_name: str, user_email: str, group: str):
     custom_objects_api = client.CustomObjectsApi(api_client)
@@ -78,11 +72,9 @@ def apply_rolebinding(user_name: str, user_email: str, group: str):
         rb_metadata_name = rb['metadata']['name']
 
         if rb_metadata_name == f"user-{user_name}-kubeflow-org-clusterrole-edit":
-            print(f"rolebinding user-{user_name}-kubeflow-org-clusterrole-edit in namespace={group} already exists")
+            logger.info(
+                f"rolebinding user-{user_name}-kubeflow-org-clusterrole-edit in namespace={group} already exists")
             create_rb = False
-
-    print(f"create_rb={create_rb}")
-
     if create_rb:
         manifest_rolebinding = {
             "apiVersion": "rbac.authorization.k8s.io/v1",
@@ -97,18 +89,17 @@ def apply_rolebinding(user_name: str, user_email: str, group: str):
             },
             "roleRef": {
                 "apiGroup": "rbac.authorization.k8s.io",
-                            "kind": "ClusterRole",
-                            "name": "kubeflow-edit"
+                "kind": "ClusterRole",
+                "name": "kubeflow-edit"
             },
             "subjects": [
                 {
                     "apiGroup": "rbac.authorization.k8s.io",
-                                "kind": "User",
-                                "name": user_email
+                    "kind": "User",
+                    "name": user_email
                 }
             ]
         }
-        print("manifest_rolebinding = " + json.dumps(manifest_rolebinding))
         rolebindings = custom_objects_api.create_namespaced_custom_object(
             group="rbac.authorization.k8s.io",
             version="v1",
@@ -117,13 +108,10 @@ def apply_rolebinding(user_name: str, user_email: str, group: str):
             body=manifest_rolebinding,
             pretty="true"
         )
-        print(inspect.getframeinfo(inspect.currentframe()).lineno)
-        print(json.dumps(rolebindings))
-        print(inspect.getframeinfo(inspect.currentframe()).lineno)
+
+
 def remove_rolebinding(user_name: str, namespace: str):
-    print(inspect.getframeinfo(inspect.currentframe()).lineno)
     custom_objects_api = client.CustomObjectsApi(api_client)
-    print(inspect.getframeinfo(inspect.currentframe()).lineno)
     rolebindings = custom_objects_api.list_namespaced_custom_object(
         group="rbac.authorization.k8s.io",
         version="v1",
@@ -131,13 +119,13 @@ def remove_rolebinding(user_name: str, namespace: str):
         plural="rolebindings",
         pretty="true"
     )
-    print(inspect.getframeinfo(inspect.currentframe()).lineno)
     # iterate over rolebindings to find if new one should be created or old one exists
     for rb in rolebindings['items']:
         rb_metadata_name = rb['metadata']['name']
 
         if rb_metadata_name == f"user-{user_name}-kubeflow-org-clusterrole-edit":
-            print(f"delete_namespaced_custom_object:rolebinding user-{user_name}-kubeflow-org-clusterrole-edit in ns={namespace}")
+            logger.info(
+                f"delete_namespaced_custom_object:rolebinding user-{user_name}-kubeflow-org-clusterrole-edit in ns={namespace}")
             try:
                 api_response = custom_objects_api.delete_namespaced_custom_object(
                     group="rbac.authorization.k8s.io",
@@ -147,11 +135,10 @@ def remove_rolebinding(user_name: str, namespace: str):
                     name=f"user-{user_name}-kubeflow-org-clusterrole-edit",
                     grace_period_seconds="0"
                 )
-                print("*****************")
-                pprint(api_response)
             except ApiException as e:
-                print("Exception when calling CustomObjectsApi->delete_namespaced_custom_object: %s\n" % e)
-            print(inspect.getframeinfo(inspect.currentframe()).lineno)
+                logger.info("Exception when calling CustomObjectsApi->delete_namespaced_custom_object: %s\n" % e)
+
+
 def apply_servicerolebinding(user_name: str, user_email: str, group: str):
     custom_objects_api = client.CustomObjectsApi(api_client)
     servicerolebindings = custom_objects_api.list_namespaced_custom_object(
@@ -168,10 +155,8 @@ def apply_servicerolebinding(user_name: str, user_email: str, group: str):
         srb_metadata_name = srb['metadata']['name']
 
         if srb_metadata_name == f"user-{user_name}-kubeflow-org-clusterrole-edit":
-            print(f"servicerolebinding user-{user_name}-kubeflow-org-clusterrole-edit already exists")
+            logger.info(f"servicerolebinding user-{user_name}-kubeflow-org-clusterrole-edit already exists")
             create_srb = False
-
-    print(f"create_srb={create_srb}")
 
     if create_srb:
         manifest_servicerolebinding = {
@@ -201,7 +186,7 @@ def apply_servicerolebinding(user_name: str, user_email: str, group: str):
             },
             "status": {}
         }
-        print("create_namespaced_custom_object:manifest_servicerolebinding = " + json.dumps(manifest_servicerolebinding))
+        logger.info(f"create_namespaced_custom_object:manifest_servicerolebinding for user: {user_email}")
         servicerolebindings = custom_objects_api.create_namespaced_custom_object(
             group="rbac.istio.io",
             version="v1alpha1",
@@ -210,7 +195,9 @@ def apply_servicerolebinding(user_name: str, user_email: str, group: str):
             body=manifest_servicerolebinding,
             pretty="true"
         )
-        print("created servicerolebindings = " + json.dumps(servicerolebindings))
+        logger.info(f"created servicerolebindings for user: {user_email}")
+
+
 def remove_servicerolebinding(user_name: str, namespace: str):
     custom_objects_api = client.CustomObjectsApi(api_client)
     servicerolebindings = custom_objects_api.list_namespaced_custom_object(
@@ -226,7 +213,8 @@ def remove_servicerolebinding(user_name: str, namespace: str):
         srb_metadata_name = srb['metadata']['name']
 
         if srb_metadata_name == f"user-{user_name}-kubeflow-org-clusterrole-edit":
-            print(f"delete_namespaced_custom_object: servicerolebinding user-{user_name}-kubeflow-org-clusterrole-edit in namespace={namespace}")
+            logger.info(
+                f"delete_namespaced_custom_object: servicerolebinding user-{user_name}-kubeflow-org-clusterrole-edit in namespace={namespace}")
             try:
                 api_response = custom_objects_api.delete_namespaced_custom_object(
                     group="rbac.istio.io",
@@ -236,10 +224,10 @@ def remove_servicerolebinding(user_name: str, namespace: str):
                     name=f"user-{user_name}-kubeflow-org-clusterrole-edit",
                     grace_period_seconds="0"
                 )
-                pprint(api_response)
             except ApiException as e:
-                print("Exception when calling CustomObjectsApi->delete_namespaced_custom_object: %s\n" % e)
-            print(inspect.getframeinfo(inspect.currentframe()).lineno)
+                logger.info("Exception when calling CustomObjectsApi->delete_namespaced_custom_object: %s\n" % e)
+
+
 def profile_exists(name, api):
     custom_objects = api.list_cluster_custom_object(
         group="kubeflow.org",
@@ -249,16 +237,18 @@ def profile_exists(name, api):
     )
     for item in custom_objects['items']:
         if item['spec']['owner']['name'] == name:
-            print(f"FOUND profile name={name}")
+            logger.info(f"FOUND profile name={name}")
             return True
-    print(f"Profile name={name} not yet found")
+    logger.info(f"Profile name={name} not yet found")
     return False
-def create_profile(username ,email, api):
+
+
+def create_profile(username, email, api):
     profile_manifest = {
         "apiVersion": "kubeflow.org/v1",
         "kind": "Profile",
         "metadata": {
-                "name": f"profile-{username}"
+            "name": f"profile-{username}"
         },
         "spec": {
             "owner": {
@@ -276,7 +266,7 @@ def create_profile(username ,email, api):
             }
         }
     }
-    print(f"profile manifest={json.dumps(profile_manifest)}")
+    logger.info(f"profile: profile-{username} created")
     api.create_cluster_custom_object(
         group="kubeflow.org",
         version="v1",
@@ -284,12 +274,14 @@ def create_profile(username ,email, api):
         body=profile_manifest,
         pretty="true"
     )
+
+
 def create_profile_ns(email, namespace, api):
     profile_manifest = {
         "apiVersion": "kubeflow.org/v1",
         "kind": "Profile",
         "metadata": {
-                "name": f"{namespace}"
+            "name": f"{namespace}"
         },
         "spec": {
             "owner": {
@@ -298,7 +290,7 @@ def create_profile_ns(email, namespace, api):
             }
         }
     }
-    print(f"profile manifest={json.dumps(profile_manifest)}")
+    logger.info(f"profile manifest for user:{email} in Namespace:{namespace}")
     api.create_cluster_custom_object(
         group="kubeflow.org",
         version="v1",
@@ -306,59 +298,57 @@ def create_profile_ns(email, namespace, api):
         body=profile_manifest,
         pretty="true"
     )
+
+
 def lambda_handler(event, context):
-    print("event = " + json.dumps(event))
-    logger.info("before namespace")
-    
+    logger.info("event = " + json.dumps(event))
+
     groups = event['request']['userAttributes'][cognito_group_field]
     data_groups = event['request']['userAttributes'][cognito_group_field]
-    groups = data_groups.replace("[","").replace("]","").split(", ")
-    groups = [element.lower() for element in groups] ; 
-    print("groups = " + json.dumps(groups))
+    groups = data_groups.replace("[", "").replace("]", "").split(", ")
+    groups = [element.lower() for element in groups];
+    logger.info("groups = " + json.dumps(groups))
     name = event['userName'].split("\\")
     if len(name) == 1:
         user_name = name[0]
     else:
         user_name = name[1]
     user_email = event['request']['userAttributes']['email']
-    
+
     namespaces = get_ns()
-    logger.info(namespaces)
-    print(f"namespaces = " + json.dumps(namespaces))
-    
-    #create the users profile due to restriction in kubeflow
+
+    # create the users profile due to restriction in kubeflow
     api = client.CustomObjectsApi(api_client)
     if not profile_exists(user_email, api):
-        print("creating profile:")
+        logger.info(f"creating profile: {user_email}")
         create_profile(user_name, user_email, api)
 
     while not profile_exists(user_email, api):
-        print(f"sleeping")
         time.sleep(2)
-    
-    #create the missing namespaces with admin owner
+
+    # create the missing namespaces with admin owner
     for group in groups:
-            if not group in namespaces:
-                    print("creating profile:")
-                    create_profile_ns("admin@kubeflow.com", group, api)
-            else:
-                print("Profile: "+group+ " already exists")
-    #refresh namespaces
-    namespaces = get_ns()           
+        if not group in namespaces:
+            logger.info("creating profile:")
+            create_profile_ns("admin@kubeflow.com", group, api)
+        else:
+            logger.info("Profile: " + group + " already exists")
+    # refresh namespaces
+    namespaces = get_ns()
     for namespace in namespaces:
-        print(f"iterating the namespace = {namespace}")
+        logger.info(f"Namespace: {namespace}")
         group = namespace
         if group in groups:
-            print(f"ENSURE rolebinding for user={user_name} nsgroup={group} user_email={user_email} namespace={namespace}")
+            logger.info(
+                f"ENSURE rolebinding for user={user_name} nsgroup={group} user_email={user_email} namespace={namespace}")
             apply_rolebinding(user_name=user_name, user_email=user_email, group=group)
-            print(f"ENSURE servicerolebinding for user={user_name} group={group} user_email={user_email} namespace={namespace}")
+            logger.info(
+                f"ENSURE servicerolebinding for user={user_name} group={group} user_email={user_email} namespace={namespace}")
             apply_servicerolebinding(user_name=user_name, user_email=user_email, group=group)
-            # print(inspect.getframeinfo(inspect.currentframe()).lineno)
         else:
-            print(f"REVOKE rolebinding user={user_name} in NAMESPACE={namespace}")
+            logger.info(f"REVOKE rolebinding user={user_name} in NAMESPACE={namespace}")
             remove_rolebinding(user_name=user_name, namespace=namespace)
-            print(f"REVOKE servicerolebinding user={user_name} in NAMESPACE={namespace}")
+            logger.info(f"REVOKE servicerolebinding user={user_name} in NAMESPACE={namespace}")
             remove_servicerolebinding(user_name=user_name, namespace=namespace)
-            # print(inspect.getframeinfo(inspect.currentframe()).lineno)
-    
+
     return event
